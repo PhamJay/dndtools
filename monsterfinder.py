f934a4a5e283ec9e5df031ea33e5d29f073c1a17
Van-Jay Pham <pha17026@spengergasse.at>
2019-03-04 15:04:43 +0100

    -Uppercased some things -Fixed a bug in encounterFinder and sandbox: Encounter-names are displayed as "encounter\\name", which breaks some code. Dirtily Fixed by splitting the String -Dirtily fixed the monsterfinder
    Signed-off-by: Van-Jay Pham <pha17026@spengergasse.at>

--- monsterfinder.py
+++ monsterfinder.py
@@ -5,14 +5,16 @@ import json
 def main():
     data = load.load_files()
     parser = argparse.ArgumentParser()
-    parser.add_argument("lookup", help="the lookup")
+    parser.add_argument("--lookup", help="the lookup")
     args = parser.parse_args()
+    print("Enter name of Monster: ")
+    args.lookup = input()
     print("looking for monster with lookup: ", args.lookup)
     res = data.search(args.lookup)
     found, key, _, certainty = res
     try:
         monster = data[key]
-        print("found a monster with a probability of ", 100 * certainty, "%", key, ":")
+        print("found a monster with a probability of ", 100 * certainty, "%: ", "'", key, "'")
         yesno = input("show monster details [Y/n]")
         if not yesno.lower() == "n":
             print(json.dumps(monster, indent=4))
